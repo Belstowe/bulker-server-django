7c44e4ed27f758b1f96b383ab2e3a7184a7cbeeb
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-02-07 21:24:42 +0700

    Test fixed (corrected view names)

--- players/tests.py
+++ players/tests.py
@@ -1,13 +1,12 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework import status
-from .models import Player
 import json
 
 # APIClient app
 client = Client()
 
-class PlayerPostTest(TestCase):
+class PostPlayersTest(TestCase):
     def setUp(self):
         self.player1valid = {
             'name': 'Uno'
@@ -26,14 +25,10 @@ class PlayerPostTest(TestCase):
             'gender': 'aa',
             'age': 20
         }
-        self.player3invalid = {
-            'id': '729e9256-7ada-4dab-a1f0-e71d5f40a8f0',
-            'name': 'Quatro'
-        }
         
     def post_response(self, payload):
         response = client.post(
-            reverse('PlayersView'),
+            reverse('Players:players_view'),
             data=json.dumps(payload),
             content_type='application/json'
         )
@@ -51,5 +46,3 @@ class PlayerPostTest(TestCase):
     def test_post_player2invalid(self):
         self.assertEqual(self.post_response(self.player2invalid), status.HTTP_400_BAD_REQUEST)
         
-    def test_post_player3invalid(self):
-        self.assertEqual(self.post_response(self.player3invalid), status.HTTP_400_BAD_REQUEST)
--- players/urls.py
+++ players/urls.py
@@ -3,11 +3,39 @@ from .views import *
 
 app_name = 'Players'
 urlpatterns = [
-    path('Players', PlayersView.as_view()),
-    path('Players/<uuid:pk>', PlayerDetail.as_view()),
-    path('Players/<uuid:pk>/Vote', PlayerVotedFor.as_view()),
-    path('Players/<uuid:pk>/Votes', PlayerVotedBy.as_view()),
-    path('Players/<uuid:subjpk>/Vote/<uuid:objpk>', PlayerVoteFor.as_view()),
-    path('AllVoted', AllVoted.as_view()),
-    path('Votes', ClearVotes.as_view())
+    path(
+        'Players',
+        PlayersView.as_view(),
+        name='players_view'
+    ),
+    path(
+        'Players/<uuid:pk>',
+        PlayerDetail.as_view(),
+        name='player_detail'
+    ),
+    path(
+        'Players/<uuid:pk>/Vote',
+        PlayerVotedFor.as_view(),
+        name='player_voted_for'
+    ),
+    path(
+        'Players/<uuid:pk>/Votes',
+        PlayerVotedBy.as_view(),
+        name='player_voted_by'
+    ),
+    path(
+        'Players/<uuid:subjpk>/Vote/<uuid:objpk>',
+        PlayerVoteFor.as_view(),
+        name='player_vote_for'
+    ),
+    path(
+        'AllVoted',
+        AllVoted.as_view(),
+        name='all_voted'
+    ),
+    path(
+        'Votes',
+        ClearVotes.as_view(),
+        name='clear_votes'
+    )
 ]
