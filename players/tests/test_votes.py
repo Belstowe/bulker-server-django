97cb27eafd1eadc57631346b4152f72a210026b2
Belreau Cohen <nuu.chah.nulth@disroot.org>
2022-02-08 15:56:14 +0700

    Test: GET, DELETE /Players/{id}/Vote

--- players/tests/test_votes.py
+++ players/tests/test_votes.py
@@ -18,11 +18,9 @@ class PlayerVotedByTest(TestCase):
         self.tres = Player.objects.create(
             name='Tres', age=81, gender='M')
         self.quatro = Player.objects.create(name='Quatro')
-
         self.uno.votedfor = self.dos
         self.dos.votedfor = self.uno
         self.tres.votedfor = self.uno
-
         self.uno.save()
         self.dos.save()
         self.tres.save()
@@ -39,7 +37,6 @@ class PlayerVotedByTest(TestCase):
                 reverse('Players:player_voted_by', kwargs={'pk': value})
             )
             self.assertEqual(response[key].status_code, status.HTTP_200_OK)
-
         self.assertEqual(len(response['uno'].data['votespree']), 2)
         self.assertTrue(self.dos.pk in response['uno'].data['votespree'])
         self.assertTrue(self.tres.pk in response['uno'].data['votespree'])
@@ -63,7 +60,6 @@ class PlayerVotedByTest(TestCase):
             reverse('Players:player_voted_by', kwargs={'pk': self.quatro.pk})
         )
         self.assertEqual(response_quatro.status_code, status.HTTP_204_NO_CONTENT)
-
         response = {}
         for key, value in {
             'uno': self.uno.pk,
@@ -86,3 +82,51 @@ class PlayerVotedByTest(TestCase):
             reverse('Players:player_voted_by', kwargs={'pk': '6b016083-77a5-4455-b318-ad70c97f2353'})
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_votedfor_get_valid(self):
+        response = {}
+        for key, value in {
+            'uno': self.uno.pk,
+            'dos': self.dos.pk,
+            'tres': self.tres.pk,
+            'quatro': self.quatro.pk
+        }.items():
+            response[key] = client.get(
+                reverse('Players:player_voted_for', kwargs={'pk': value})
+            )
+        self.assertEqual(response['uno'].status_code, status.HTTP_200_OK)
+        self.assertEqual(response['uno'].data['name'], self.dos.name)
+        self.assertEqual(response['dos'].status_code, status.HTTP_200_OK)
+        self.assertEqual(response['dos'].data['name'], self.uno.name)
+        self.assertEqual(response['tres'].status_code, status.HTTP_200_OK)
+        self.assertEqual(response['tres'].data['name'], self.uno.name)
+        self.assertEqual(response['quatro'].status_code, status.HTTP_204_NO_CONTENT)
+        
+    def test_votedfor_get_invalid(self):
+        response = client.get(
+            reverse('Players:player_voted_for', kwargs={'pk': '6b016083-77a5-4455-b318-ad70c97f2353'})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_votedfor_delete_valid(self):
+        response_uno = client.delete(
+            reverse('Players:player_voted_for', kwargs={'pk': self.uno.pk})
+        )
+        self.assertEqual(response_uno.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_uno.data['votespree']), 0)
+        response_dos = client.delete(
+            reverse('Players:player_voted_for', kwargs={'pk': self.dos.pk})
+        )
+        self.assertEqual(response_dos.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response_dos.data['votespree']), 1)
+        self.assertTrue(self.tres.pk in response_dos.data['votespree'])
+        response_quatro = client.delete(
+            reverse('Players:player_voted_for', kwargs={'pk': self.quatro.pk})
+        )
+        self.assertEqual(response_quatro.status_code, status.HTTP_204_NO_CONTENT)
+        
+    def test_votedfor_delete_invalid(self):
+        response_uno = client.delete(
+            reverse('Players:player_voted_for', kwargs={'pk': '6b016083-77a5-4455-b318-ad70c97f2353'})
+        )
+        self.assertEqual(response_uno.status_code, status.HTTP_404_NOT_FOUND)
--- players/views.py
+++ players/views.py
@@ -157,6 +157,9 @@ class PlayerVotedFor(APIView):
                 status=status.HTTP_404_NOT_FOUND
             )
         object_record = subject_record.votedfor
+        
+        if object_record is None:
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
         serializer_clear = self.subject_serializer(
             subject_record,
